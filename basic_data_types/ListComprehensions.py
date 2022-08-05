ae1e8f93f4936f2c2b0baa094b78f744eddf9d6f
Mahimai Raja J <mahimairaja3@gmail.com>
2022-08-05 06:03:44 +0530

    Using List comprehension in python3

--- basic_data_types/ListComprehensions.py
+++ basic_data_types/ListComprehensions.py
@@ -1,20 +1,18 @@
-''' Question
+# Question List Comprehension
 
-Let's learn about list comprehensions! You are given three integers  and representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
-Example 
- 
- 
- 
+# Let's learn about list comprehensions! You are given three integers  and representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+# Example 
 
-All permutations of  are: 
-.
-Print an array of the elements that do not sum to .
 
-Input Format
-Four integers  and , each on a separate line.
+# All permutations of  are: 
+# .
+# Print an array of the elements that do not sum to .
 
-Constraints
-Print the list in lexicographic increasing order.'''
+# Input Format
+# Four integers  and , each on a separate line.
+
+# Constraints
+# Print the list in lexicographic increasing order.
 
 if __name__ == '__main__':
     x = int(input())
