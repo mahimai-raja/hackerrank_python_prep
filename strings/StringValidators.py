5657463acbf243e6a7599aaa2efbfc0198c37ac9
Mahimai Raja J <mahimairaja3@gmail.com>
2022-08-20 11:04:31 +0530

    Validation of built in functions

---
+++ strings/StringValidators.py
@@ -0,0 +1,18 @@
+# Task
+
+# You are given a string S. 
+# Your task is to find out if the string S contains: 
+# alphanumeric characters, alphabetical characters, digits, 
+# lowercase and uppercase characters.
+
+# --------------------------------------------------------
+
+if __name__ == '__main__':
+    s = input()
+    print(any(map(str.isalnum,s)))
+    print(any(map(str.isalpha,s)))
+    print(any(map(str.isdigit,s)))
+    print(any(map(str.islower,s)))
+    print(any(map(str.isupper,s)))
+    
+    
